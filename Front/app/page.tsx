0a19d0c9459f87dec35a3f13048f5c77822e2e82
Miguel Gomez <mikeagf300@gmail.com>
2024-12-04 15:25:45 -0600

    se añadio component hero y modifico tailwindconfig para que utilice correctamente los estilos

--- Front/app/page.tsx
+++ Front/app/page.tsx
@@ -1,3 +1,10 @@
+import Hero from '../components/Hero/Hero';
+
 export default function Home() {
-  return <h1>Home Peludopolis</h1>
+  return (
+    <>
+    <Hero/>
+    <h1>Home Peludopolis</h1>
+    </>
+    )
 }
