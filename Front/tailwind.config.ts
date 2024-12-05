7d19e098d6ae2ceeee893e9e54a4a4654ab2eddd
Marcosotoladev <marcosotoladev@gmail.com>
2024-12-05 00:38:49 -0300

    instalacion de dependencias y fuentes

--- Front/tailwind.config.ts
+++ Front/tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import { Config } from 'tailwindcss';
 
 export default {
   content: [
@@ -11,8 +11,23 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#F9A826", // Naranja claro
+        secondary: "#A7C7E7", // Azul claro
+        success: "#98FB98", // Verde menta
+        danger: "#FF7F50", // Coral
+        warning: "#FFD700", // Amarillo brillante
+        info: "#FADADD", // Rosa pastel
+        light: "#F5F5DC", // Beige
+        dark: "#D2B48C", // Marrón claro
+        muted: "#F0F0F0", // Gris claro
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"], // Texto principal
+        title: ["Poppins", "sans-serif"], // Títulos
+        fun: ["Chewy", "cursive"], // Letras divertidas
       },
     },
   },
   plugins: [],
 } satisfies Config;
+
