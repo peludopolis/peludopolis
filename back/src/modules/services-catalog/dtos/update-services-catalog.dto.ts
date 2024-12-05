6b44b61ca56577f0433f1cec56169bb7da10012e
ledesmafcu <ledesma@gmail.com>
2024-12-05 17:54:42 -0300

    Creacion de entities y dtos para service-catalog

---
+++ back/src/modules/services-catalog/dtos/update-services-catalog.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateServicesCatalogDto } from "./create-services-catalog.dto";
+
+export class UpdateServicesCatalogDto extends PartialType(CreateServicesCatalogDto) {}
