757956360dc3bed26a7df4fca0c9b854a9c06a11
Miguel Gomez <mikeagf300@gmail.com>
2024-12-06 11:46:50 -0600

    Se añadieron cardspara los servicios de las mascotas en el home, se corrigio la importacion de hero que marcaba error

--- Front/app/page.tsx
+++ Front/app/page.tsx
@@ -1,10 +1,12 @@
-import Hero from '../components/hero/Hero';
+import Hero from '../components/Hero/Hero';
+import ServicesPets from "../components/ServicesPets/page"
 
 export default function Home() {
   return (
     <>
     <Hero/>
     <h1>Home Peludopolis</h1>
+    <ServicesPets/>
     </>
     )
 }
