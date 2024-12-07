a0f17614afafb60eed9de2bf75f0cf3091551b11
Miguel Torres <miguel.torres99@outlook.com>
2024-12-07 02:47:55 -0500

    interceptor para datos sensibles de usuarios

---
+++ back/src/modules/users/interceptor/removeSensitiveData.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class RemoveSensitiveFieldsInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        if (Array.isArray(data)) {
+          return data.map((user) => this.removeSensitiveFields(user));
+        } else if (data && typeof data === 'object') {
+          return this.removeSensitiveFields(data);
+        }
+        return data;
+      })
+    );
+  }
+
+  private removeSensitiveFields(user: Record<string, any>) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, isAdmin, ...userResponse } = user;
+    return userResponse;
+  }
+}
