1e5dea11937a5563a2a856027b4af01a0ecbda56
Marcosotoladev <marcosotoladev@gmail.com>
2024-12-12 23:40:04 -0300

    se agrego link a location en nav menu

--- Front/components/Navbar/navMenu/navItems.ts
+++ Front/components/Navbar/navMenu/navItems.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavItem } from './constants';
-import { Home, Sun, Calendar, Users, Lightbulb } from 'lucide-react';
+import { Home, Sun, Calendar, Users, Lightbulb, MapPin } from 'lucide-react';
 
 export const navItems: NavItem[] = [
     { href: '/', label: 'Inicio', soundKey: 'inicio', icon: React.createElement(Home, { size: 24, color: '#1DA1F2' }) },
@@ -8,6 +8,7 @@ export const navItems: NavItem[] = [
     { href: '/appointments', label: 'Turnos', soundKey: 'turnos', icon: React.createElement(Calendar, { size: 24, color: '#28A745' }) },
     { href: '/comunity', label: 'Comunidad', soundKey: 'comunidad', icon: React.createElement(Users, { size: 24, color: '#FFC107' }) },
     { href: '/tips', label: 'Tips', soundKey: 'tips', icon: React.createElement(Lightbulb, { size: 24, color: '#DC3545' }) },
+    { href: '/location', label: 'Ubicación', soundKey: 'turnos', icon: React.createElement(MapPin, { size: 24, color: '#1DA1A0' }) },
     { href: '/about', label: 'Nosotros', soundKey: 'nosotros', icon: React.createElement(Users, { size: 24, color: '#6F42C1' }) }
 ];
 
