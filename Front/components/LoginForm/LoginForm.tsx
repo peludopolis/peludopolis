cde8b916452e2a127d38204e8690926830997471
Miguel Gomez <mikeagf300@gmail.com>
2024-12-17 11:29:21 -0600

    Implementación de inicio de sesión con Google y redirección al dashboard

--- Front/components/LoginForm/LoginForm.tsx
+++ Front/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { GoogleLogin } from "@react-oauth/google";
 import { AuthContext } from "../../contexts/authContext";
 import { login } from "../../service/auth";
 import { useRouter } from "next/navigation";
@@ -13,7 +14,9 @@ const LoginForm = () => {
     const [data, setData] = useState({ email: "", password: "" });
     const [errors, setErrors] = useState({ email: "", password: "" });
     const [touched, setTouched] = useState({ email: false, password: false });
+    const [isLoginWithGoogle, setIsLoginWithGoogle] = useState(false); // Estado para cambiar entre login
 
+    // Manejador del submit con email y contraseña
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const res = await login(data);
@@ -26,30 +29,70 @@ const LoginForm = () => {
         }
     };
 
-    const handleGoogleLogin = () => {
-        window.location.href = "http://localhost:3001/login"; // Redirige al endpoint de login con Google
+    // Manejador del login con Google
+    const handleGoogleSuccess = (credentialResponse: any) => {
+        const token = credentialResponse.credential;
+    
+        // Decodifica el token para obtener la información del usuario
+        const payload = JSON.parse(atob(token.split('.')[1]));  // Decodifica el JWT
+    
+        // Guarda la sesión en el localStorage
+        localStorage.setItem('googleToken', token);
+        localStorage.setItem('user', JSON.stringify({
+            user: {
+                name: payload.name,
+                email: payload.email,
+                picture: payload.picture,
+                id: undefined,  // Puedes agregar otros campos si los tienes
+            },
+            login: true,
+        }));
+    
+        // Actualiza el estado global del usuario
+        setUser({
+            user: {
+                name: payload.name,
+                email: payload.email,
+                picture: payload.picture,
+                id: undefined,
+            },
+            login: true,
+        });
+    
+        alert('Inicio de sesión exitoso');
+        router.push('/');
+    };
+    
+
+    const handleGoogleFailure = () => {
+        alert("Error al iniciar sesión con Google");
     };
 
+    // Manejador de cambios en los inputs
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setData({ ...data, [e.target.name]: e.target.value });
     };
 
+    // Manejador de onBlur para marcar si el campo fue tocado
     const handleBlur = (e: ChangeEvent<HTMLInputElement>) => {
         setTouched({ ...touched, [e.target.name]: true });
     };
 
+    // Validación de email
     const validateEmail = (e: string) => {
         let validation = "";
         if (!validator.isEmail(e)) validation = "Wrong email address";
         return validation;
     };
 
+    // Validación de contraseña
     const validatePassword = (p: string) => {
         let validation = "";
         if (!validator.isLength(p, { min: 4, max: 8 })) validation = "Min 4, max 8";
         return validation;
     };
 
+    // Actualización de errores en los campos
     useEffect(() => {
         setErrors({
             email: validateEmail(data.email),
@@ -64,55 +107,63 @@ const LoginForm = () => {
                 <h3 className="text-black">
                     Si ya eres parte de Peludópolis, inicia sesión con tu correo electrónico y contraseña o utiliza Google.
                 </h3>
-                <label className="block mb-2 text-sm font-medium mt-4 text-black" htmlFor="">
-                    Usuario
-                </label>
-                <input
-                    type="text"
-                    className="bg-white border border-gray-300 text-black-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-black"
-                    placeholder="name@email.com"
-                    name="email"
-                    onChange={(e) => handleChange(e)}
-                    value={data.email}
-                    onBlur={(e) => handleBlur(e)}
-                />
-                {touched.email && <p className="text-black">{errors.email}</p>}
+
+                {/* Formulario con usuario y contraseña */}
+                {!isLoginWithGoogle ? (
+                    <>
+                        <label className="block mb-2 text-sm font-medium mt-4 text-black" htmlFor="email">
+                            Usuario
+                        </label>
+                        <input
+                            type="text"
+                            className="bg-white border border-gray-300 text-black-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-black"
+                            placeholder="name@email.com"
+                            name="email"
+                            onChange={(e) => handleChange(e)}
+                            value={data.email}
+                            onBlur={(e) => handleBlur(e)}
+                        />
+                        {touched.email && <p className="text-black">{errors.email}</p>}
+
+                        <label className="block mb-2 text-sm font-medium text-black" htmlFor="password">
+                            Contraseña
+                        </label>
+                        <input
+                            type="password"
+                            className="bg-white border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            placeholder="********"
+                            name="password"
+                            onChange={(e) => handleChange(e)}
+                            value={data.password}
+                            onBlur={(e) => handleBlur(e)}
+                        />
+                        {touched.password && <p className="text-black">{errors.password}</p>}
+
+                        <button
+                            type="submit"
+                            className="text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-3"
+                        >
+                            Iniciar Sesión
+                        </button>
+                    </>
+                ) : (
+                    // Login con Google
+                    <GoogleLogin
+                        onSuccess={handleGoogleSuccess}
+                        onError={handleGoogleFailure}
+                    />
+                )}
+
+                <div className="flex gap-3 mt-3">
+                    <button
+                        type="button"
+                        onClick={() => setIsLoginWithGoogle(!isLoginWithGoogle)} // Cambia entre los dos métodos de login
+                        className="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-bold rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                    >
+                        {isLoginWithGoogle ? "Usar correo y contraseña" : "Iniciar sesión con Google"}
+                    </button>
+                </div>
             </div>
-
-            <div>
-                <label className="block mb-2 text-sm font-medium text-black" htmlFor="">
-                    Contraseña
-                </label>
-                <input
-                    type="password"
-                    className="bg-white border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                    placeholder="password"
-                    name="password"
-                    onChange={(e) => handleChange(e)}
-                    value={data.password}
-                    onBlur={(e) => handleBlur(e)}
-                />
-                {touched.password && <p className="text-black">{errors.password}</p>}
-            </div>
-
-            <button
-                type="submit"
-                className="text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-            >
-                Iniciar Sesión
-            </button>
-
-            <button
-                type="button"
-                onClick={handleGoogleLogin}
-                className="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-bold rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-3"
-            >
-                Iniciar Sesión con Google
-            </button>
-
-            <a href="/forgot-password" className="text-blue-500 hover:text-blue-700 text-sm mt-2 inline-block">
-                ¿Olvidaste tu contraseña?
-            </a>
         </form>
     );
 };
--- Front/components/Navbar/greeting/Greeting.tsx
+++ Front/components/Navbar/greeting/Greeting.tsx
@@ -6,15 +6,22 @@ import { AuthContext } from '../../../contexts/authContext';
 const Greeting = () => {
     const { user } = useContext(AuthContext);
 
-    if (!user) return null;
-
     return (
-        <div>
-            <h1 className='ml-2 text-danger'>
-                {user.user.name}
+        <div className="flex items-center">
+            {user?.user?.picture && (
+                <img 
+                    src={user.user.picture} 
+                    alt="Profile" 
+                    className="w-10 h-10 rounded-full mr-3" 
+                />
+            )}
+            <h1 className="ml-2 text-danger">
+                {user?.user?.name}
             </h1>
         </div>
     );
 };
 
 export default Greeting;
+
+
