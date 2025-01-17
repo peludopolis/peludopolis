cc69afd65ed3e77f3e5e8142e14c46d6e1525db8
Miguel Torres <miguel.torres99@outlook.com>
2025-01-16 23:46:00 -0500

    correción para deploy, prueba de base de datos en render

--- back/src/config/typeOrm.config.ts
+++ back/src/config/typeOrm.config.ts
@@ -1,44 +1,45 @@
-import { registerAs } from '@nestjs/config';
-import { config as dotenvConfig } from 'dotenv';
-import { DataSource, DataSourceOptions } from 'typeorm';
-
-dotenvConfig({ path: '.env' });
-
-const config = {
-  type: 'postgres',
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  autoLoadEntities: true,
-  synchronize: true,
-  logging: true,
-  dropSchema: false,
-  entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations: ['dist/migrations/*{.js,.ts}']
-};
-
-export default registerAs('typeorm', () => config);
-
-export const connectionSource = new DataSource(config as DataSourceOptions);
-
 // import { registerAs } from '@nestjs/config';
 // import { config as dotenvConfig } from 'dotenv';
 // import { DataSource, DataSourceOptions } from 'typeorm';
 
 // dotenvConfig({ path: '.env' });
 
-// const config: DataSourceOptions = {
+// const config = {
 //   type: 'postgres',
-//   url: process.env.DATABASE_URL,
+//   database: process.env.DB_NAME,
+//   host: process.env.DB_HOST,
+//   port: Number(process.env.DB_PORT),
+//   username: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   autoLoadEntities: true,
 //   synchronize: true,
 //   logging: true,
-//   migrations: ['dist/migrations/*{.js,.ts}'],
+//   dropSchema: false,
 //   entities: ['dist/**/*.entity{.ts,.js}'],
-//   ssl: { rejectUnauthorized: false }
+//   migrations: ['dist/migrations/*{.js,.ts}']
 // };
 
 // export default registerAs('typeorm', () => config);
 
-// export const connectionSource = new DataSource(config);
+// export const connectionSource = new DataSource(config as DataSourceOptions);
+
+import { registerAs } from '@nestjs/config';
+import { config as dotenvConfig } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenvConfig({ path: '.env' });
+
+const config: DataSourceOptions = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  synchronize: true,
+  logging: true,
+  dropSchema: false,
+  migrations: ['dist/migrations/*{.js,.ts}'],
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  ssl: { rejectUnauthorized: false }
+};
+
+export default registerAs('typeorm', () => config);
+
+export const connectionSource = new DataSource(config);
